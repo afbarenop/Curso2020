27e6d0f7608df99e3e7ba40bad2385c69ca99aef
Andres <afbarenop@unal.edu.co>
2020-03-26 20:16:09 -0500

    Primer programa

---
+++ Primos.py
@@ -0,0 +1,20 @@
+x=1
+y=1
+w=1
+while (w < 1000):
+    while (y <= x):
+        z= x%y
+        if (z==0):
+            if (y==x):
+                w= w+1
+                print(x)
+            break
+        y=y+1
+    x=x+1
+    y=2
+
+
+
+
+
+        
